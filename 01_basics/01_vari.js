7ff3ec0b0aae8d66d346f5c7d3bb0c6b6a7fb18f
Gagan kumar D <146530709+Gagankumard@users.noreply.github.com>
2024-01-09 02:39:31 +0000

    datatypes

---
+++ 01_basics/01_vari.js
@@ -0,0 +1,9 @@
+let caBee="gag";
+const a=123;
+var b ="helloo";
+
+caBee="kum";
+// a=12121;
+b="byee";
+
+console.table([caBee,a,b]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+// Number:1 ,2
+// String =" dc "
+// boolean=>true/false
+// null => standalone value
+// undefined=>  example-- let a;
+// symbol=> unique
+
+
+
+let a="gagan";
+
+// Object
+
+console.log(typeof("gagan"));
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
