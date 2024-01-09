210dae5ef1ffd9597e4a0e44fab9742523a5c902
Gagan kumar D <146530709+Gagankumard@users.noreply.github.com>
2024-01-09 03:07:37 +0000

    conersion of DataTypes

---
+++ 01_basics/03_conversionOperaton.js
@@ -0,0 +1,30 @@
+let score=21;
+// let score="32scb";
+// let score =null;
+// let score=true;
+
+let valueNum = Number(score)
+console.log(typeof valueNum);
+console.log(valueNum);
+
+// Nan -- > not a Number
+
+
+// "33"->33
+// "33abs"->NaN
+// true ->1 , false->0
+// "gagan"=true
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// --> true
+
+
+let helloBe=33;
+
+let stringNum=String(helloBe);
+console.log(typeof stringNum);
+// --> string
